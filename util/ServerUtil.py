0b72fede473903143951465f1f94f4baa28e02bf
Samuel <ifeanyisamuel1900@gmail.com>
2025-09-23 05:16:21 +0100

    Update and rename server_util.py to ServerUtil.py

---
+++ util/ServerUtil.py
@@ -0,0 +1,17 @@
+import os
+import httpx
+
+class ServerUtil:
+    @staticmethod
+    async def get_google_access_token() -> str:
+        url = "https://oauth2.googleapis.com/token"
+        payload = {
+            "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+            "client_secret": os.getenv("GOOGLE_CLIENT_SECRET"),
+            "refresh_token": os.getenv("GOOGLE_REFRESH_TOKEN"),
+            "grant_type": "refresh_token"
+        }
+        async with httpx.AsyncClient() as client:
+            resp = await client.post(url, data=payload)
+            resp.raise_for_status()
+            return resp.json()["access_token"]
